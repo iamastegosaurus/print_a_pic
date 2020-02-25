be5bbe7a49422ea15e967784c9d3a0c0b4605428
thespahrtan <ebspahr@gmail.com>
2020-02-25 15:58:10 -0600

    testing

--- color/test.py
+++ color/test.py
@@ -4,7 +4,21 @@
 
 import cv2
 
+img = cv2.imread('Q:\\print_a_pic\\color\\resized.jpg')
+copy = img.copy()
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+h, w, _ = img.shape
 
 
+for y in range(h):
+    for x in range(w):
+        hsv[y][x][1] += 50
 
+back_to_bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+
+cv2.imshow('og', copy)
+cv2.imshow('modified', back_to_bgr)
+cv2.waitKey(0)
+
+cv2.imwrite('Q:\\print_a_pic\\color\\satboost50.jpg', back_to_bgr)
 
