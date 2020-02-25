f3eef16466b6433f857e2ffad182d9e34bfe9778
thespahrtan <ebspahr@gmail.com>
2020-02-25 14:22:06 -0600

    to get color info

---
+++ color/test.py
@@ -0,0 +1,10 @@
+# original dim: 936 x 793 (w, h) = 742,248
+# downscaled to: 504 x 427 (w, h) = 215,208
+# arc and plane downloads match original dim (936, 793) and each other
+
+import cv2
+
+
+
+
+
