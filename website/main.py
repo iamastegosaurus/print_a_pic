5429cc378209062540d03779e43d111712fd5a43
thespahrtan <ebspahr@gmail.com>
2020-04-02 17:36:18 -0500

    moving towards website 1.0

--- website/main.py
+++ website/main.py
@@ -1,19 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 import os
 import json 
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def home():
 
-    os.system("blender --background --python " + path + "\\printapic.py")
+    # os.system("blender --background --python " + path + "\\printapic.py")
+    # with open(path + "\\settings.json") as f:
+    #     config = json.load(f)
+    # return f"completed in {config['time']} seconds"
 
-    with open(path + "\\settings.json") as f:
-        config = json.load(f)
-
-    return f"completed in {config['time']} seconds"
+    if request.method == 'POST':
+        filename = request.form['content']
+        return filename
+    else:
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
