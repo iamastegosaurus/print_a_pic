fb0b930bef2e6bd530cf063b1f6b9998ad9e5d28
Eric Spahr <ebspahr@gmail.com>
2020-04-07 21:32:20 -0500

    big changes boi

--- printapic.py
+++ printapic.py
@@ -95,10 +95,10 @@ if mode == 'panorama':
 bpy.ops.transform.resize(value=(scale, scale, scale), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
 bpy.ops.transform.rotate(value= -90*deg_rad, orient_axis='X', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
 
-bpy.ops.export_mesh.stl(filepath = path + '\\myfile.stl')
+name = os.path.splitext(image)[0]
+bpy.ops.export_mesh.stl(filepath = path + '\\results\\' + name + '.stl')
 
 completion_time = time.time() - start
-print(completion_time)
 
 config['time'] = round(completion_time, 4)
 
--- runn.py
+++
@@ -1,3 +0,0 @@
-import os
-path = os.path.dirname(os.path.abspath(__file__))
-os.system("blender --background --python " + path + "\\printapic.py")
