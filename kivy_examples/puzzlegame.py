b17778fef64e6e15cb3e2b92645bbf611731a774
Eric Spahr <ebspahr@gmail.com>
2020-05-14 15:29:09 -0500

    working puzzle game

--- kivy_examples/puzzlegame.py
+++ kivy_examples/puzzlegame.py
@@ -1,7 +1,3 @@
-import os
-os.environ["KIVY_NO_CONSOLELOG"] = "warning"
-import kivy
-
 from kivy.app import App
 from kivy.uix.camera import Camera
 from kivy.uix.widget import Widget
@@ -9,15 +5,20 @@ from kivy.uix.scatter import Scatter
 from kivy.animation import Animation
 from kivy.graphics import Color, Rectangle
 from kivy.properties import NumericProperty
+from kivy.config import Config
 from random import randint, random
-from functools import partial
-
-from kivy.core.window import Window
-
 
 class Puzzle(Camera):
 
-    blocksize = NumericProperty(200)
+    w = 5
+    h = 4
+    bs = 150
+
+    blocksize = NumericProperty(bs)
+    width = NumericProperty(w)
+    height = NumericProperty(h)
+
+    emptySquare = ( (w-1)*bs, (h-1)*bs )
 
     def on_texture_size(self, instance, value):
         self.build()
@@ -28,11 +29,8 @@ class Puzzle(Camera):
     def build(self):
         texture = self.texture
         bs = self.blocksize
-        tw, th = self.texture_size
-        # print(tw, th)
-        print(int(tw / bs), int(th / bs))
-        for x in range(int(tw / bs)):
-            for y in range(int(th / bs)):
+        for x in range(self.width): 
+            for y in range(self.height):
                 bx = x * bs
                 by = y * bs
                 subtexture = texture.get_region(bx, by, bs, bs)
@@ -40,41 +38,51 @@ class Puzzle(Camera):
                 with node.canvas:
                     Color(1, 1, 1)
                     Rectangle(size=node.size, texture=subtexture)
-                self.add_widget(node)
-
+                if x+1 == self.width and y == 0:
+                    pass
+                else:
+                    self.add_widget(node)
         self.shuffle()
 
+
     def shuffle(self):
-        texture = self.texture
-        bs = self.blocksize
-        tw, th = self.texture_size
-        count = int(tw / bs) * int(th / bs) #- 1
+        count = (self.width * self.height) - 1
         indices = list(range(count))
         childindex = 0
         while indices:
             index = indices.pop(randint(0, len(indices) - 1))
-            x = bs * (index % int(tw / bs))
-            y = bs * int(index / int(tw / bs))
+            x = self.blocksize * (index % self.width)
+            y = self.blocksize * int(index / self.width)
             child = self.children[childindex]
-            a = Animation(d=random() / 4.) + Animation(pos=(x, y),
-                                                       t='out_quad', d=.4)
+            a = Animation(d=random() / 4.) + Animation(pos=(x, y), t='out_quad', d=.4)
             a.start(child)
             childindex += 1
+    
 
     def on_touch_down(self, touch):
-        print(touch.pos)
+        x = int(touch.pos[0] / self.blocksize) * self.blocksize
+        y = int(touch.pos[1] / self.blocksize) * self.blocksize
+
+        for c in range( (self.width * self.height) - 1 ):
+            child = self.children[c]
+            if child.pos[0] == x and child.pos[1] == y:
+                a = Animation(d=random() / 4.) + Animation(pos=self.emptySquare, t='out_quad', d=.4)
+                a.start(child)
+                self.emptySquare = (x, y)
+                break
+
         if touch.is_double_tap:
             self.shuffle()
             return True
-        super(Puzzle, self).on_touch_down(touch)
-
 
 class PuzzleApp(App):
     def build(self):
-
+        Config.set('graphics', 'width', '750')
+        Config.write()
         root = Widget()
-        puzzle = Puzzle(resolution=(1200, 800), play=True)
+        puzzle = Puzzle(resolution=(900, 900), play=True)
         root.add_widget(puzzle)
+
         return root
 
 PuzzleApp().run()
